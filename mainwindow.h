ce5cab79742bb8a53464c5c5e0d4409d211eb38c
Zulektemp <zulek24@mail.ru>
2014-11-11 15:06:50 +0900

    first reposit
    не читает русский язык

---
+++ mainwindow.h
@@ -0,0 +1,36 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlError>
+#include <QtSql/QSqlQuery>
+#include <QtSql/QSqlTableModel>
+
+
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    QSqlDatabase db;
+
+private:
+    Ui::MainWindow *ui;
+    QSqlTableModel *m_csvModel;
+
+    void setupModel();
+    void fillTableView();
+
+    
+};
+
+#endif // MAINWINDOW_H
